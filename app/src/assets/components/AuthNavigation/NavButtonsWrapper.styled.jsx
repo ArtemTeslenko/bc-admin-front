0ada944464b299b26dcc584888fdd20ee6186b47
pti4ka121 <pti4ka121@gmail.com>
2024-05-31 18:27:43 +0300

    bc-front-2: create button styles add calculator to utils and create inputs

---
+++ app/src/assets/components/AuthNavigation/NavButtonsWrapper.styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const NavButtonsWrapper = styled.div`
+display: flex;
+justify-content: flex-end;
+gap: 10px;
+`
---
+++ app/src/assets/reusable/CommonButton.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CommonButton = styled.button`
+  padding: 8px 12px;
+  font-size: 16px;
+  line-height: 1;
+  color: #ffffff;
+  background-color: #474747;
+  border: none;
+  border-radius: 6px;
+  &:hover{
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+`;
