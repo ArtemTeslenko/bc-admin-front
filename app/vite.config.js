d8fcb2895ed91fb3ed8ae970c5ea29dadd5c72f5
Artem <104884717+ArtemTeslenko@users.noreply.github.com>
2024-06-26 23:28:39 +0300

    main: build app

--- app/vite.config.js
+++ app/vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src/") }],
   },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "/src/main.js",
+    },
+  },
 });
